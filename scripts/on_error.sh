0f334300e4fbb243a93a4dabf65ad1a45a945f86
zoff99 <zoff@zoff.cc>
2019-09-21 09:40:54 +0200

    use led0

--- scripts/on_error.sh
+++ scripts/on_error.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # set led to blinking
-echo heartbeat | sudo tee /sys/class/leds/led1/trigger
+echo heartbeat | sudo tee /sys/class/leds/led0/trigger
--- scripts/on_start.sh
+++ scripts/on_start.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 # set led to steady light (yes its "none" on the pi zero!)
-echo none | sudo tee /sys/class/leds/led1/trigger
+echo none | sudo tee /sys/class/leds/led0/trigger
