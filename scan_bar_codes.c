dfa35399d8ac2f5eaf445112263447dc7da940fa
zoff99 <zoff@zoff.cc>
2019-10-02 14:45:52 +0200

    incr. version

--- scan_bar_codes.c
+++ scan_bar_codes.c
@@ -50,8 +50,8 @@
 // ----------- version -----------
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 99
-#define VERSION_PATCH 0
-static const char global_version_string[] = "0.99.0";
+#define VERSION_PATCH 1
+static const char global_version_string[] = "0.99.1";
 // ----------- version -----------
 // ----------- version -----------
 
